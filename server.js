3c4b356f23c253f705b6102f51605d16a02bc5cf
Wendell Yamada <mrwendell@gmail.com>
2022-11-21 16:35:49 -0800

    create UserPageComponent

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const HOST = '127.0.0.1'
 
 const server = net.createServer()
 
+const UserPageComponent = () => 'GET / HTTP/1.1\n\n<html><body><h1>Hello User</h1></body></html>'
+
 server.listen(PORT, HOST, () => {
     console.log(`Listening on ${HOST}:${PORT}`)
 })
@@ -11,7 +13,8 @@ server.listen(PORT, HOST, () => {
 server.on('connection', (socket) => {
     console.log('connection created')
     socket.on('data', (data) => {
-        console.log(`${socket.remoteAddress}:BLARG ${data}`)
+        console.log(`${data}`)
+        socket.write(UserPageComponent())
     })
-    socket.write('no dicks yet')
 }) 
+
